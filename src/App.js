cade42dfdc72a1d0a54090ba970247986d4b7abf
Steven Zuluaga <steven000069@hotmail.com>
2022-10-02 23:43:30 -0500

    Creando Carrusel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import './App.css'
 
-function App() {
+const App = () => {
+  const images = [
+    "https://cdn.pixabay.com/photo/2015/02/02/11/09/office-620822_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2015/01/08/18/27/startup-593341_960_720.jpg",
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slide>
+
+      {
+        images.map((image, index) => (
+          <div>
+            <div key={index} className='each-slide-effect' >
+              <img src={image} className='image' />
+
+              <a href='#'>Ver más</a>
+            </div>
+          </div>
+        )
+        )
+      }
+    </Slide>
   );
-}
+};
 
 export default App;
